8220b040a96f62da0e2d02b01a1072e57d83dc26
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2015-12-17 02:28:13 +0530

    lines

--- src/main/java/uk/co/sangharsh/nlp/resource/NlpResource.java
+++ src/main/java/uk/co/sangharsh/nlp/resource/NlpResource.java
@@ -2,6 +2,7 @@ package uk.co.sangharsh.nlp.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,8 +18,14 @@ public class NlpResource {
 	private Summarizer summarizer; 
 	
 	@GET
-	@Path("summarize")
-	public String summarize() {
-		return summarizer.test();
+	@Path("summarize/{lines}")
+	public String summarize(@PathParam(value = "lines") int lines) {
+		String content = content();		
+		return summarizer.summarize(content, lines);
+	}
+
+	private String content() {
+		String content = "India, officially the Republic of India (Bhārat Gaṇarājya),[18][19][c] is a country in South Asia. It is the seventh-largest country by area, the second-most populous country with over 1.2 billion people, and the most populous democracy in the world. India is a federal constitutional republic governed under a parliamentary system consisting of 29 states and 7 union territories. A pluralistic, multilingual, and multi-ethnic society, the country is also home to a diversity of wildlife in a variety of protected habitats. Bounded by the Indian Ocean on the south, the Arabian Sea on the south-west, and the Bay of Bengal on the south-east, it shares land borders with Pakistan to the west;[d] China, Nepal, and Bhutan to the north-east; and Myanmar (Burma) and Bangladesh to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; in addition, India's Andaman and Nicobar Islands share a maritime border with Thailand and Indonesia.Home to the ancient Indus Valley Civilisation and a region of historic trade routes and vast empires, the Indian subcontinent was identified with its commercial and cultural wealth for much of its long history.[20] Four major religions—Hinduism, Buddhism, Jainism, and Sikhism—originated here, whereas Zoroastrianism and the Abrahamic religions of Judaism, Christianity, and Islam arrived in the first millennium CE and also shaped the region's diverse culture. Gradually brought under the administration of the British East India Company from the early 18th century and administered directly by the United Kingdom after the Indian Rebellion of 1857, India became an independent nation in 1947 after a struggle for independence led by Mahatma Gandhi that was marked by non-violent resistance. Upon the promulgation of its constitution, India became a republic on 26 January 1950.Following market-based economic reforms in 1991, India became one of the fastest-growing major economies and is considered a newly industrialised country. The Indian economy is the world's seventh-largest by nominal GDP and third-largest by purchasing power parity (PPP).[14] It has the third-largest standing army in the world and ranks ninth in military expenditure among nations, while being recognised as a nuclear weapons state and regional power. However, it continues to face the challenges of widespread poverty, corruption, malnutrition and inadequate public health.";
+		return content;
 	}
 }
--- src/main/java/uk/co/sangharsh/nlp/service/Summarizer.java
+++ src/main/java/uk/co/sangharsh/nlp/service/Summarizer.java
@@ -2,6 +2,4 @@ package uk.co.sangharsh.nlp.service;
 
 public interface Summarizer {
 	String summarize(String document, int numOfSentences);
-
-	String test();
 }
