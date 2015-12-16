cf504ed95d62ce31bbb8d895209756805a9f668d
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2015-12-17 02:22:52 +0530

    Service integration

--- src/main/java/uk/co/sangharsh/nlp/resource/NlpResource.java
+++ src/main/java/uk/co/sangharsh/nlp/resource/NlpResource.java
@@ -3,16 +3,22 @@ package uk.co.sangharsh.nlp.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import uk.co.sangharsh.nlp.service.Summarizer;
 
 @Component
 @Path(NlpResource.ROOT)
 public class NlpResource {
 	public static final String ROOT = "nlp";
 
+	@Autowired
+	private Summarizer summarizer; 
+	
 	@GET
 	@Path("summarize")
 	public String summarize() {
-		return "ABC";
+		return summarizer.test();
 	}
 }
---
+++ src/main/java/uk/co/sangharsh/nlp/service/SummarizerImpl.java
@@ -0,0 +1,127 @@
+package uk.co.sangharsh.nlp.service;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.stats.ClassicCounter;
+import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.util.CoreMap;
+
+@Service
+public class SummarizerImpl implements Summarizer {
+
+	private StanfordCoreNLP pipeline;
+
+	private final Counter<String> dfCounter;
+	private final int numDocuments;
+	 
+	public SummarizerImpl(){
+		this.dfCounter = new ClassicCounter<String>();
+	    this.numDocuments = (int) dfCounter.getCount("__all__");
+	}
+	@PostConstruct
+	public void setNlp() {
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize,ssplit,pos");
+		props.setProperty("tokenize.language", "en");
+		props.setProperty(
+				"pos.model",
+				"edu/stanford/nlp/models/pos-tagger/english/english-bidirectional-distsim.tagger");
+
+		pipeline = new StanfordCoreNLP(props);
+	}
+
+	@Override
+	public String summarize(String document, int numSentences) {
+		Annotation annotation = pipeline.process(document);
+		List<CoreMap> sentences = annotation
+				.get(CoreAnnotations.SentencesAnnotation.class);
+
+		Counter<String> tfs = getTermFrequencies(sentences);
+		sentences = rankSentences(sentences, tfs);
+
+		StringBuilder ret = new StringBuilder();
+		for (int i = 0; i < numSentences; i++) {
+			ret.append(sentences.get(i));
+			ret.append(" ");
+		}
+
+		return ret.toString();
+	}
+
+	private static Counter<String> getTermFrequencies(List<CoreMap> sentences) {
+		Counter<String> ret = new ClassicCounter<String>();
+
+		for (CoreMap sentence : sentences)
+			for (CoreLabel cl : sentence
+					.get(CoreAnnotations.TokensAnnotation.class))
+				ret.incrementCount(cl.get(CoreAnnotations.TextAnnotation.class));
+
+		return ret;
+	}
+
+	private List<CoreMap> rankSentences(List<CoreMap> sentences,
+			Counter<String> tfs) {
+		Collections.sort(sentences, new SentenceComparator(tfs));
+		return sentences;
+	}
+	private class SentenceComparator implements Comparator<CoreMap> {
+
+	    private final Counter<String> termFrequencies;
+
+	    public SentenceComparator(Counter<String> termFrequencies) {
+	      this.termFrequencies = termFrequencies;
+	    }
+
+	    @Override
+	    public int compare(CoreMap o1, CoreMap o2) {
+	      return (int) Math.round(score(o2) - score(o1));
+	    }
+
+	    /**
+	     * Compute sentence score (higher is better).
+	     */
+	    private double score(CoreMap sentence) {
+	      double tfidf = tfIDFWeights(sentence);
+
+	      // Weight by position of sentence in document
+	      int index = sentence.get(CoreAnnotations.SentenceIndexAnnotation.class);
+	      double indexWeight = 5.0 / index;
+
+	      return indexWeight * tfidf * 100;
+	    }
+
+	    private double tfIDFWeights(CoreMap sentence) {
+	      double total = 0;
+	      for (CoreLabel cl : sentence.get(CoreAnnotations.TokensAnnotation.class))
+	        if (cl.get(CoreAnnotations.PartOfSpeechAnnotation.class).startsWith("n"))
+	          total += tfIDFWeight(cl.get(CoreAnnotations.TextAnnotation.class));
+
+	      return total;
+	    }
+
+	    private double tfIDFWeight(String word) {
+	      if (dfCounter.getCount(word) == 0)
+	        return 0;
+
+	      double tf = 1 + Math.log(termFrequencies.getCount(word));
+	      double idf = Math.log(numDocuments / (1 + dfCounter.getCount(word)));
+	      return tf * idf;
+	    }
+	  
+	}
+	@Override
+	public String test() {
+		return this.getClass().getName();
+	}
+}
