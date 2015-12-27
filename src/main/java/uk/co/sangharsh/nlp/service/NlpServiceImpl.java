7aa1d18e481ef66ff6b4655dd8806d14b9beb591
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2015-12-27 14:53:36 +0530

    Test

--- src/main/java/uk/co/sangharsh/nlp/service/NlpServiceImpl.java
+++ src/main/java/uk/co/sangharsh/nlp/service/NlpServiceImpl.java
@@ -1,12 +1,8 @@
 package uk.co.sangharsh.nlp.service;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
 
@@ -23,6 +19,7 @@ import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.stats.ClassicCounter;
 import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.util.ArrayCoreMap;
 import edu.stanford.nlp.util.CoreMap;
 
 @Service
@@ -35,14 +32,12 @@ public class NlpServiceImpl implements NlpService {
 	private StanfordCoreNLP pipeline;
 
 	private Counter<String> dfCounter;
-	private int numDocuments;
 	
 	private static final String SERIALIZED_CLASSIFIER = NLP_RESOURCE + "classifiers/english.all.3class.distsim.crf.ser.gz";
 	 
 	@PostConstruct
 	public void setNlp() throws ClassNotFoundException, IOException {
-		this.dfCounter = loadDfCounter(DF_COUNTER_PATH);
-	    this.numDocuments = (int) dfCounter.getCount("__all__");
+		this.dfCounter = ObjectUtil.loadObjectNoExceptions(DF_COUNTER_PATH, Counter.class);
 	    
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize,ssplit,pos");
@@ -54,12 +49,6 @@ public class NlpServiceImpl implements NlpService {
 		pipeline = new StanfordCoreNLP(props);
 	}
 
-	private Counter<String> loadDfCounter(String path)throws IOException, ClassNotFoundException {
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(path)));
-		Counter<String> readObject = (Counter<String>) ois.readObject();
-		ois.close();
-		return readObject;
-	}
 	@Override
 	public List<String> summarize(String document, int numSentences) {
 		List<String> result = new ArrayList<String>();
@@ -74,13 +63,34 @@ public class NlpServiceImpl implements NlpService {
 		}
 		return result;
 	}
+	private List<CoreLabel> makeSentence(String sentence) {
+	    String[] words = sentence.split(" ");
+	    List<CoreLabel> labels = new ArrayList<CoreLabel>();
+	    for (String word : words) {
+	      CoreLabel label = new CoreLabel();
+	      label.setWord(word);
+	      label.setValue(word);
+	      labels.add(label);
+	    }
+	    return labels;
+	  }
 	@Override
 	public List<String> summarize(Conversation conversation, int numSentences) {
-		StringBuilder builder = new StringBuilder();
+		List<String> result = new ArrayList<String>();
+		List<CoreMap> sentences = new ArrayList<>();
 		for(Utterance utterance : conversation.utterances()){
-			builder.append(utterance.speaker()).append(" said \"").append(utterance.text());
+			CoreMap map = new ArrayCoreMap(1);
+			String string = new StringBuilder().append(utterance.speaker()).append(" said \"").append(utterance.text()).append("\" ").toString();
+			map.set(CoreAnnotations.TokensAnnotation.class, makeSentence(string));
+			sentences.add(map);
 		}
-		return summarize(builder.toString(), numSentences);
+		Counter<String> tfs = getTermFrequencies(sentences);
+		sentences = rankSentences(sentences, tfs);
+		int max = Math.min(numSentences, sentences.size());
+		for (CoreMap sentence: sentences.subList(0, max-1)) {
+			result.add(sentence.toString());
+		}
+		return result;
 	}
 	private static Counter<String> getTermFrequencies(List<CoreMap> sentences) {
 		Counter<String> ret = new ClassicCounter<String>();
@@ -95,55 +105,8 @@ public class NlpServiceImpl implements NlpService {
 
 	private List<CoreMap> rankSentences(List<CoreMap> sentences,
 			Counter<String> tfs) {
-		Collections.sort(sentences, new SentenceComparator(tfs));
+		Collections.sort(sentences, new SentenceComparator(dfCounter, tfs));
 		return sentences;
-	}
-	private class SentenceComparator implements Comparator<CoreMap> {
-
-	    private final Counter<String> termFrequencies;
-
-	    public SentenceComparator(Counter<String> termFrequencies) {
-	      this.termFrequencies = termFrequencies;
-	    }
-
-	    @Override
-	    public int compare(CoreMap o1, CoreMap o2) {
-	      return (int) Math.round(score(o2) - score(o1));
-	    }
-
-	    /**
-	     * Compute sentence score (higher is better).
-	     */
-	    private double score(CoreMap sentence) {
-	      double tfidf = tfIDFWeights(sentence);
-
-	      // Weight by position of sentence in document
-	      int index = sentence.get(CoreAnnotations.SentenceIndexAnnotation.class);
-	      double indexWeight = 5.0 / index;
-
-	      return indexWeight * tfidf * 100;
-	    }
-
-	    private double tfIDFWeights(CoreMap sentence) {
-	      double total = 0;
-	      for (CoreLabel cl : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-			String pos = cl.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-			if (PosUtil.isNoun(pos))
-	          total += tfIDFWeight(cl.get(CoreAnnotations.TextAnnotation.class));
-		}
-
-	      return total;
-	    }
-
-	    private double tfIDFWeight(String word) {
-	      if (dfCounter.getCount(word) == 0)
-	        return 0;
-
-	      double tf = 1 + Math.log(termFrequencies.getCount(word));
-	      double idf = Math.log(numDocuments / (1 + dfCounter.getCount(word)));
-	      return tf * idf;
-	    }
-	  
 	}
 	@Override
 	public List<String> recognizeNamedEntity(String text) {
